228771107c117f658b1400d76b9b7640c90e8e01
魏柯岩 <keyan.wei@qingclass.com>
2017-08-29 15:37:25 +0800

    初始化

--- src/main/java/com/example/RestfulDemo/RestfulDemoApplication.java
+++ src/main/java/com/example/RestfulDemo/RestfulDemoApplication.java
@@ -2,10 +2,20 @@ package com.example.RestfulDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.boot.autoconfigure.*;
 
-@SpringBootApplication
+
+@Controller
+@EnableAutoConfiguration
 public class RestfulDemoApplication {
-
+	@RequestMapping("/hello")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(RestfulDemoApplication.class, args);
 	}
